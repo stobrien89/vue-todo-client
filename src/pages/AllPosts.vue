90491d2628303c782c1c2d307b4c68bab51ff5c9
stobrien89 <stobrien@crimson.ua.edu>
2021-10-09 09:32:29 -0700

    add structure to allposts page

--- src/pages/AllPosts.vue
+++ src/pages/AllPosts.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>AllPosts</h1>
+  <div>
+    <div class="post" v-for="(post, index) in $attrs.posts" v-bind:key="index">
+      <router-link :to="{ name: 'post', params: { id: index } }">
+        <h1>{{ post.subject }}</h1>
+      </router-link>
+      <h2>{{ post.details }}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +15,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.post {
+  text-align: center;
+  border: 3px solid green;
+  margin: 10px auto;
+  width: 80%;
+}
+</style>
