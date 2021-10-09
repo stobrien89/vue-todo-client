76efa79b3e61828636698c834c90b686209464cf
stobrien89 <stobrien@crimson.ua.edu>
2021-10-09 08:15:21 -0700

    add routes/router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createWebHistory, createRouter } from "vue-router";
+import routes from "./routes";
+import "milligram";
 
-createApp(App).mount('#app')
+//Create our Router
+const router = createRouter({
+  // Create a web history
+  history: createWebHistory(),
+  // inject our routes
+  routes,
+});
+
+//Create our vue application
+const app = createApp(App);
+
+// inject our router into our app
+app.use(router);
+
+// Mount our App to the DOM
+app.mount("#app");
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+/////////////////////////
+// Components Imports
+/////////////////////////
+
+/////////////////////////
+// Array of Routes
+/////////////////////////
+export default [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+  ];
