ebe4c7590c8a3f8a39ab32e82da5c43cfaf188b2
stobrien89 <stobrien@crimson.ua.edu>
2021-10-09 08:23:03 -0700

    add pages

---
+++ src/pages/AllPosts.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>AllPosts</h1>
+</template>
+
+<script>
+export default {
+  name: "AllPost",
+};
+</script>
+
+<style></style>
---
+++ src/pages/SinglePost.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>SinglePost</h1>
+</template>
+
+<script>
+export default {
+  name: "SinglePost",
+};
+</script>
+
+<style></style>
