634f21bb670be0a94bfadeb4a6bfd075df38963c
stobrien89 <stobrien@crimson.ua.edu>
2021-10-09 08:59:28 -0700

    add routes for pages

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 /////////////////////////
-// Components Imports
+// Component Imports
 /////////////////////////
-
+import AllPosts from "./pages/AllPosts.vue";
+import SinglePost from "./pages/SinglePost.vue";
+import Form from "./pages/Form.vue";
 /////////////////////////
 // Array of Routes
 /////////////////////////
 export default [
-    { path: "/", component: Home },
-    { path: "/about", component: About },
+    // main page that shows all todos
+    { path: "/", component: AllPosts },
+    // page for seeing an individual todo
+    { path: "/post/:id", component: SinglePost, name: "post" },
+    // route for creating a new todo
+    { path: "/new", component: Form, name: "new" },
+    // route for updating a todo
+    { path: "/edit", component: Form, name: "edit" },
   ];
